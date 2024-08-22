4f318507225474115c5a99ba22cacf92b5404b1d
Frisco Rose <friscorose@gmail.com>
2024-08-21 22:54:33 -0400

    WIP for glyph engine with tabular, kerning and hinting

--- src/textual/renderables/digits.py
+++ src/textual/renderables/digits.py
@@ -4,65 +4,14 @@ from rich.console import Console, ConsoleOptions, RenderResult
 from rich.measure import Measurement
 from rich.segment import Segment
 from rich.style import Style, StyleType
+
 from pkgutil import get_data as LoadGlyphs
 import json
 import math
 
-DIGITS = " 0123456789+-^x:"
-DIGITS3X3 = """\
-
-
-
-┏━┓
-┃ ┃
-┗━┛
- ┓
- ┃
-╺┻╸
-╺━┓
-┏━┛
-┗━╸
-╺━┓
- ━┫
-╺━┛
-╻ ╻
-┗━┫
-  ╹
-┏━╸
-┗━┓
-╺━┛
-┏━╸
-┣━┓
-┗━┛
-╺━┓
-  ┃
-  ╹
-┏━┓
-┣━┫
-┗━┛
-┏━┓
-┗━┫
-╺━┛
-
-╺╋╸
-
-
-╺━╸
-
- ^
-
-
-
- ×
-
-
- :
-
-""".splitlines()
-
 
 class Digits:
-    """Renders a 3X3 unicode 'font' for numerical values.
+    """Renders a wXh unicode glyph 'font' for input token charactes.
 
     Args:
         text: Text to display.
@@ -70,14 +19,17 @@ class Digits:
 
     """
 
+
     def __init__(self, text: str, style: StyleType = "") -> None:
         self._text = text
         self._style = style
 
 
-    font = "box/sans/default.json"
-    glyph_faces = LoadGlyphs('textual' , 'renderables/glyphs/' + font )
+    glyph_family = "box/sans/default.json"
+    glyph_faces = LoadGlyphs('textual' , 'renderables/glyphs/' + glyph_family )
     GLYPHS = json.loads( glyph_faces )
+    height = GLYPHS['fixed lines']
+    width = GLYPHS['fixed columns']
 
     def __rich_console__(
         self, console: Console, options: ConsoleOptions
@@ -85,14 +37,94 @@ class Digits:
         style = console.get_style(self._style)
         yield from self.render(style)
 
-    def get_glyph(self, token: str) -> None:
-        my_glyph = self.GLYPHS['character'][ token ]['glyph']
+    def en_glyph(self, text: str, style: Style) -> RenderResult:
+        """Engine to layout glyph strings in supercell with style
+
+        Args:
+            text: Text to display.
+            style: Rich Style.
+
+        Returns:
+            2D list of glyph strings.
+        """
+
         try:
-            my_glyph = my_glyph['bold']
+            bbox_height = self.GLYPHS['fixed lines']
+            bbox_width = self.GLYPHS['fixed columns']
+            bbox_align = self.GLYPHS['align']
+            bbox_kerning = self.GLYPHS['kerning']
+            bbox_tabular = self.GLYPHS['tabular']
+            faces_data = self.GLYPHS['character']
         except:
-            pass
-        return my_glyph
-            
+            raise Exception("missing required glyph face data")
+
+        was_space = True
+        g_strings: list[list[str]] =[[] for i in range(1, bbox_height+1)]
+
+        for token in text:
+            default = {"columns":1,"lines":1,"glyph":[token]}
+            face = faces_data.get(token, default)
+            Thint = face.get('tabular', bbox_tabular)
+            Khint = face.get('kerning', bbox_kerning)
+            Hhint = face.get('lines', bbox_height)
+            Whint = face.get('columns', bbox_width)
+            Ahint = face.get('align', bbox_align)
+            face = face.get('glyph', face )
+            if isinstance( face, dict ):
+                if style.bold:
+                    face = face.get('bold', face )
+                else:
+                    face = face.get('normal', face )
+
+            #determine horizontal glyph placement in supercell
+            if Thint:
+                if Ahint[0] == "left":
+                    l_pad = 0
+                    r_pad = bbox_width - Whint
+                elif Ahint[0] == "right":
+                    l_pad = bbox_width - Whint
+                    r_pad = 0
+                else:
+                    pad = (bbox_width - Whint)/2.0
+                    if was_space:
+                        l_pad = math.ceil(pad)
+                        r_pad = math.floor(pad)
+                    else:
+                        l_pad = math.floor(pad)
+                        r_pad = math.ceil(pad)
+            else:
+                l_pad = r_pad = 0
+            r_pad += math.ceil( Khint )
+
+            #determine vertical glyph placement in supercell
+            if Hhint == bbox_height:
+                t_pad = b_pad = 0
+            else:
+                if Ahint[1] == "top":
+                    t_pad = 0
+                    b_pad = bbox_height - Hhint
+                elif Ahint[1] == "bottom":
+                    t_pad = bbox_height - Hhint
+                    b_pad = 0
+                else:
+                    pad = (bbox_height - Hhint)/2.0
+                    t_pad = math.ceil(pad)
+                    b_pad = math.floor(pad)
+
+            #construct glyph supercell sequence
+            for n, g_row in enumerate( g_strings ):
+                if n < t_pad or n >= t_pad+Hhint:
+                    g_data = " "*Whint
+                else:
+                    g_data = face[n - t_pad] 
+                g_row.append( " "*l_pad + g_data + " "*r_pad )
+
+            was_space = token == " "
+            #process next token or finished
+
+        self.height = len( g_strings )
+        self.width = len( g_strings[0] )
+        return g_strings
 
     def render(self, style: Style) -> RenderResult:
         """Render with the given style
@@ -103,71 +135,7 @@ class Digits:
         Returns:
             Result of render.
         """
-        monospace = True
-        was_space = True
-
-        try:
-            bbox_height = self.GLYPHS['fixed lines']
-            bbox_width = self.GLYPHS['fixed columns']
-            bbox_align = self.GLYPHS['fixed align']
-            faces_data = self.GLYPHS['character']
-        except:
-            raise Exception("missing required glyph face data")
-
-        g_strings: list[list[str]] =[[] for i in range(1, bbox_height+1)]
-
-        for token in self._text:
-            face = faces_data.get(token, token)
-            Hhint = face.get('lines', bbox_height)
-            Whint = face.get('columns', bbox_width)
-            Ahint = face.get('align', bbox_align)
-            face = face.get('glyph', face )
-            if isinstance( face, dict ):
-                face = face.get('bold', face )
-
-            if Whint == bbox_width:
-                l_pad = r_pad = 0
-            else:
-                if Ahint[0] == "left":
-                    l_pad = 0
-                    r_pad = bbox_width - Whint
-                elif Ahint[0] == "right":
-                    r_pad = 0
-                    l_pad = bbox_width - Whint
-                else:
-                    pad = (bbox_width - Whint)/2.0
-                    if was_space:
-                        l_pad = math.ceil(pad)
-                        r_pad = math.floor(pad)
-                    else:
-                        r_pad = math.ceil(pad)
-                        l_pad = math.floor(pad)
-            if token != " ":
-                was_space = False
-            else: 
-                was_space = True
-
-            if Hhint == bbox_height:
-                t_pad = b_pad = 0
-            else:
-                if Ahint[1] == "top":
-                    t_pad = 0
-                    b_pad = bbox_height - Hhint
-                elif Ahint[1] == "bottom":
-                    b_pad = 0
-                    t_pad = bbox_height - Hhint
-                else:
-                    pad = (bbox_height - Hhint)/2.0
-                    t_pad = math.ceil(pad)
-                    b_pad = math.floor(pad)
-
-            for n, g_row in enumerate( g_strings ):
-                if n < t_pad or n >= t_pad+Hhint:
-                    g_row.append( " "*Whint )
-                else:
-                    g_row.append( " "*l_pad + face[n - t_pad] + " "*r_pad )
-
-        for g_row in g_strings:
+        for g_row in self.en_glyph( self._text, style):
             yield Segment("".join(g_row), style)
             yield Segment.line()
 
@@ -184,8 +152,7 @@ class Digits:
             width of the text (in cells).
         """
         #Read from the max of default or tcss glyphs fixed height 
-        height = 3
-        return height
+        return cls.height
 
     @classmethod
     def get_width(cls, text: str) -> int:
@@ -197,8 +164,7 @@ class Digits:
         Returns:
             width of the text (in cells).
         """
-        width = sum(3 if character in DIGITS else 1 for character in text)
-        return width
+        return cls.width
 
     def __rich_measure__(
         self, console: Console, options: ConsoleOptions
